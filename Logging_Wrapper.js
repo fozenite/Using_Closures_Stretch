f3c4741c15b5c25d8cc7293111c4791142a24a54
Rohit <moza.rohit@gmail.com>
2017-04-06 21:23:20 +0000

    Accessing arguments array in Javascript

--- Logging_Wrapper.js
+++ Logging_Wrapper.js
@@ -1,9 +1,29 @@
+
+// WRAP LOG TAKES IN A FUNCTION/ CALLBACK AND A STRING
+// ADDITIONALLY LOGS THE NAME , INPUT PARAMETERS, AND RETURN VALUES
+// OF THE CALLBACK FUNCTION
+
 var wrapLog = function (callback, name) {
   /* your code here */
+   // result = callback.apply(callback,arguments);
+    //console.log(result);
+    return function(){
+
+
+       var args = Array.prototype.slice.call(arguments);
+       var args = [].slice.call(arguments);
+
+        result = callback.apply(callback,arguments);
+        console.log(name+"("+ args[0] + "," + args[1] + ")" + " => " + result);
+
+    };
+
 };
 
 var sum = function (a,b) { return a + b; };
 var logSum = wrapLog(sum, "sum");
 
-logSum(5,3); // sum(5,3) => 8
-logSum(3,2); // sum(3,2) => 5
+
+logSum(5,3);
+logSum(2,3);
+logSum(5,4);
